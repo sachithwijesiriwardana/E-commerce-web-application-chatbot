957c215ca85e8219431ec036c402162e68e6f1fa
Sachith Wijeisriwardana <88984823+sachithwijesiriwardana@users.noreply.github.com>
2023-05-17 16:26:25 +0530

    Delete avms directory

--- avms/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 2.0
-    </div>
-    Copyright &copy; <?php echo date("Y");?> <strong> Apartment Visitor Management System </strong>
-  </footer>
